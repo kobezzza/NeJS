23a1725126e72df982d281accc1a929495215724
kobezzza <kobezzza@mail.ru>
2013-07-05 12:09:02 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: kobezzza
+ * Date: 05.07.13
+ * Time: 12:03
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ rebuild.bat
@@ -0,0 +1,1 @@
+call jossy ./lib/core.js > nejs.js
