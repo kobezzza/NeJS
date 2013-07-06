8631bfa33fd06761e5625a86882a53cfd6eddd3d
kobezzza <kobezzza@mail.ru>
2013-07-06 18:37:47 +0400

    Вроде рабочая версия

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --harmony
 
 var NeJS = require('./nejs');
 var Program = require('commander');
 
 Program
-	.version('0.0.1')
+	.version('0.0.2')
 	.option('-s, --source [src]', 'source file')
 	.option('-o, --output [src]', 'output file')
 	.parse(process.argv);
@@ -12,37 +12,39 @@ Program
 var fs = require('fs');
 var file = Program.source;
 var newFile = Program.output || (file + '.js');
+var defs = require(defs);
 
 fs.readFile(file, function (err, data) {
 	if (err) {
 		console.log(err);
 
 	} else {
-		fs.writeFile(newFile, NeJS.compile(String(data)), function (err) {
+		var res = defs(NeJS.compile(String(data)), {
+			"environments": ["node", "browser"],
+			"disallowVars": false,
+			"disallowDuplicated": false,
+			"disallowUnknownReferences": false
+		});
+
+		if (res.exitcode !== 0) {
+			process.exit(res.exitcode);
+		}
+
+		if (res.stats) {
+			console.log(res.stats.toString());
+			process.exit(0);
+		}
+
+		if (res.ast) {
+			console.log(JSON.stringify(res.ast, null, 4));
+		}
+
+		fs.writeFile(newFile, res.src, function (err) {
 			if (err) {
 				console.log(err);
 
 			} else {
-				var defs = require('child_process').spawn('node', [
-					'--harmony',
-					__dirname + '/node_modules/defs/defs',
-					newFile
-				]);
-
-				defs.stderr.on('data', function (data) {
-					throw data;
-				});
-
-				defs.stdout.on('data', function (data) {
-					fs.writeFile(newFile, data, function (err) {
-						if (err) {
-							console.log(err);
-
-						} else {
-							console.log('File "' + file + '" has been successfully compiled (' + newFile + ').');
-						}
-					});
-				});
+				console.log('File "' + file + '" has been successfully compiled (' + newFile + ').');
 			}
 		});
 	}
