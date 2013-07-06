dba4babacee55689c0788d2ca7dcc3e0d9a64a3a
kobezzza <kobezzza@mail.ru>
2013-07-06 17:10:06 +0400

    Вроде рабочая версия

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-/**
- * Created with JetBrains WebStorm.
- * User: kobezzza
- * Date: 05.07.13
- * Time: 12:03
- * To change this template use File | Settings | File Templates.
- */
+#!/usr/bin/env node
+
+var NeJS = require('./nejs');
+var Program = require('commander');
+
+Program
+	.version('0.0.1')
+	.option('-s, --source [src]', 'source file')
+	.option('-o, --output [src]', 'output file')
+	.parse(process.argv);
+
+var fs = require('fs');
+var file = Program.source;
+var newFile = Program.output || (file + '.js');
+
+fs.readFile(file, function (err, data) {
+	if (err) {
+		console.log(err);
+
+	} else {
+		fs.writeFile(newFile, NeJS.compile(String(data)), function (err) {
+			if (err) {
+				console.log(err);
+
+			} else {
+				console.log('File "' + file + '" has been successfully compiled (' + newFile + ').');
+			}
+		});
+	}
+});
